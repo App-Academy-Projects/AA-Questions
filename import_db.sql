77bec246aa2d3c3c26032bf5a3c79c5a1551f74a
usamaeltmsah <usamaeltmsah@gmail.com>
2020-11-27 00:03:01 +0200

    Handle question_follows foreign keys to be refrences to its tables, as well as adding the primary key

--- import_db.sql
+++ import_db.sql
@@ -12,14 +12,18 @@ CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    associated_author_id INTEGER NOT NULL,
+    author_id INTEGER NOT NULL,
 
-    FOREIGN KEY (associated_author_id) REFERENCES users(id)
+    FOREIGN KEY (author_id) REFERENCES users(id)
 )
 
 DROP TABLE IF EXISTS question_follows;
 
 CREATE TABLE question_follows (
+    id INTEGER PRIMARY KEY,
     question_id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL
+    user_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (question_id) REFERENCES questions(id)
 )
