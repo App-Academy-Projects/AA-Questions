77c958cd38ab19e367a0d2cb51aad6cee69f59ad
usamaeltmsah <usamaeltmsah@gmail.com>
2020-11-27 01:14:48 +0200

    SEEDING THE DATABASE TABLES

--- import_db.sql
+++ import_db.sql
@@ -51,3 +51,54 @@ CREATE TABLE question_likes (
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
+-- SEEDING THE DATABASE
+
+-- USERS
+INSERT INTO
+    users (fname, lname)
+VALUES
+    ("Ali", "Ahmed"), ("John", "Mic"), ("Mark", "Bob");
+
+SELECT * FROM users;
+
+-- QUESTIONS
+INSERT INTO
+  questions (title, body, author_id)
+VALUES
+    ("TITLE OF FIRST QUESTION", "FIRST QUESTION BODY", 1),
+    ("TITLE OF SECOND QUESTION", "SECOND QUESTION BODY", 1),
+    ("TITLE OF THIRD QUESTION", "THIRD QUESTION BODY", 2),
+    ("TITLE OF FOURTH QUESTION", "FOURTH QUESTION BODY", 3);
+
+SELECT * FROM questions;
+
+-- QUESTION_FOLLOWS
+INSERT INTO
+  question_follows (user_id, question_id)
+VALUES
+  (1, 1),
+  (1, 2),
+  (2, 1),
+  (3, 2),
+  (3, 3);
+
+SELECT * FROM question_follows;
+
+-- REPLIES
+INSERT INTO
+  replies (question_id, parent_reply_id, author_id, body)
+VALUES
+    (2, 1, 2, "BODY OF REPLY ON REPLY WITH ID = 2 ON QUESTION WITH ID = 2"),
+    (1, NULL, 3, "BODY OF REPLY ON QUESTION WITH ID = 2"),
+    (3, 2, 3, "BODY OF REPLY ON REPLY WITH ID = 3 ON QUESTION WITH ID = 3");
+
+SELECT * FROM replies;
+
+-- QUESTION_LIKES
+INSERT INTO
+  question_likes (user_id, question_id)
+VALUES
+    (1, 3), (2, 1), (1, 2), (3, 2), (3, 3);
+
+SELECT * FROM question_likes;
