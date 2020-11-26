50ea1ce1c2f2b9eff796cfd08d397a3637e9eb36
usamaeltmsah <usamaeltmsah@gmail.com>
2020-11-26 22:52:45 +0200

    Create table questions if not existing

--- import_db.sql
+++ import_db.sql
@@ -5,3 +5,14 @@ CREATE TABLE users (
     fname TEXT NOT NULL,
     lname TEXT NOT NULL
 );
+
+DROP TABLE IF EXISTS questions;
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    associated_author_id INTEGER NOT NULL,
+
+    FOREIGN KEY (associated_author_id) REFERENCES users(id)
+)
