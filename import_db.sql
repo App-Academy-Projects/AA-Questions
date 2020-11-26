0c320742b3dd86e18d20eb662d59ebedcfb42b0c
usamaeltmsah <usamaeltmsah@gmail.com>
2020-11-27 00:19:26 +0200

    Create question_likes table if not exists

--- import_db.sql
+++ import_db.sql
@@ -15,7 +15,7 @@ CREATE TABLE questions (
     author_id INTEGER NOT NULL,
 
     FOREIGN KEY (author_id) REFERENCES users(id)
-)
+);
 
 DROP TABLE IF EXISTS question_follows;
 
@@ -26,7 +26,7 @@ CREATE TABLE question_follows (
 
     FOREIGN KEY (user_id) REFERENCES users(id)
     FOREIGN KEY (question_id) REFERENCES questions(id)
-)
+);
 
 DROP TABLE IF EXISTS replies;
 
@@ -40,4 +40,15 @@ CREATE TABLE replies (
     FOREIGN KEY (question_id) REFERENCES questions(id),
     FOREIGN KEY (parent_reply_id) REFERENCES replies(id),
     FOREIGN KEY (author_id) REFERENCES users(id)
-)
+);
+
+DROP TABLE IF EXISTS question_likes;
+
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    user_id  INTEGER NOT NULL,
+    question_id  INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
