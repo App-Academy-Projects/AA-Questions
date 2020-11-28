6f7f093f13ade02da50ff35735a08d8d2d24f101
usamaeltmsah <usamaeltmsah@gmail.com>
2020-11-28 14:00:40 +0200

    Get question by id

--- questions.rb
+++ questions.rb
@@ -18,8 +18,22 @@ class Question
         @year = options['body']
         @playwright_id = options['author_id']
     end
+
     def self.all
         data = QuestionsDatabase.instance.execute("SELECT * FROM questions")
         data.map { |datum| Question.new(datum) }
+    end
+
+    def self.find_by_id(id)
+        question = QuestionsDatabase.instance.execute(<<-SQL, id)
+        SELECT
+            *
+        FROM
+            questions
+        WHERE
+            id = ?
+        SQL
+        return nil if question.empty?
+        Question.new(question.first)
     end
 end
