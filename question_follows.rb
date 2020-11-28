66c41c540c5161bbc72d086377dcbaad41728687
usamaeltmsah <usamaeltmsah@gmail.com>
2020-11-28 22:59:44 +0200

    Get followers of a question by its id

--- question_follows.rb
+++ question_follows.rb
@@ -7,4 +7,19 @@ class QuestionFollow
         @question_id = options['question_id']
         @user_id = options['user_id']
     end
+
+    def self.followers_for_question_id(question_id)
+        followers = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+        SELECT
+            users.*
+        FROM
+            users
+        JOIN
+            question_follows ON users.id = user_id
+        WHERE
+            question_follows.question_id = ?
+        SQL
+        return nil if followers.empty?
+        followers.map { |follower| User.new(follower) }
+    end
 end
