1303870493d3416ada079ce8e6537ec4aff2855d
usamaeltmsah <usamaeltmsah@gmail.com>
2020-11-28 14:37:22 +0200

    Get users by name

--- user.rb
+++ user.rb
@@ -7,4 +7,17 @@ class User
         @fname = options['fname']
         @lname = options['lname']
     end
+
+    def self.find_by_name(fname, lname)
+        users = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+        SELECT
+            *
+        FROM
+            users
+        WHERE
+            fname = ? AND lname = ?
+        SQL
+        return nil if users.empty?
+        users.map { |user| User.new(user) }
+    end
 end
